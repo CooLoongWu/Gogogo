48e02f5291112b94757733a9c642b47d7835f8d6
CooLoongWu <742420210@qq.com>
2018-03-06 14:52:39 +0800

    流程控制-Select

--- src/main/Select.go
+++ src/main/Select.go
@@ -1,7 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	ch4 := make(chan int, 1)
+	for i := 0; i < 4; i++ {
+		select {
+		case e, ok := <-ch4:
+			fmt.Println("=====", i)
+			if !ok {
+				fmt.Println("End.")
+				return
+			}
+			fmt.Println(e)
+
+			close(ch4)
+		default:
+			fmt.Println("=====", i)
+			fmt.Println("No Data!")
+			ch4 <- 1
+		}
+	}
 }
 
 /*
