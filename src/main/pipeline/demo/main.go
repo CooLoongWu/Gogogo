443c4b010bf101f2204666594da8c88359f89614
CooLoongWu <742420210@qq.com>
2018-03-08 17:13:29 +0800

    流程控制-GOPATH的坑

--- src/main/pipeline/demo/main.go
+++ src/main/pipeline/demo/main.go
@@ -1,7 +1,22 @@
-package demo
+package main
 
-//import "Gogogo/src/main/pipeline/node"
-//
-//func main() {
-//	p := node.ArraySource(1, 3, 5, 8, 0)
-//}
+import (
+	"main/pipeline/node"
+	"fmt"
+)
+
+func main() {
+	p := node.ArraySource(1, 3, 5, 8, 0)
+	p = node.InMemSort(p)
+	//for {
+	//	if num, ok := <-p; ok {
+	//		fmt.Println(num)
+	//	} else {
+	//		break
+	//	}
+	//}
+
+	for v := range p {
+		fmt.Println(v)
+	}
+}
--- src/main/pipeline/node/nodes.go
+++ src/main/pipeline/node/nodes.go
@@ -1,6 +1,9 @@
 package node
 
-func ArraySource(a ...int) chan int {
+import "sort"
+
+// <-chan 只能输出的管道
+func ArraySource(a ...int) <-chan int {
 	out := make(chan int)
 	go func() {
 		for _, v := range a {
@@ -8,5 +11,24 @@ func ArraySource(a ...int) chan int {
 		}
 		close(out)
 	}()
+	return out
+}
+
+func InMemSort(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		var a []int
+		for v := range in {
+			a = append(a, v)
+		}
+
+		sort.Ints(a)
+
+		for _, v := range a {
+			out <- v
+		}
+		close(out)
+	}()
+
 	return out
 }
