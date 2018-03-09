5fba9ea4c2f77503b171125ffb3e7205f2f257a6
CooLoongWu <742420210@qq.com>
2018-03-09 15:55:51 +0800

    流程控制-网络排序

--- src/main/pipeline/demo/sort.go
+++ src/main/pipeline/demo/sort.go
@@ -5,14 +5,19 @@ import (
 	"main/pipeline/node"
 	"bufio"
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	p := createPipeline("small.in",
-		512,
-		4)
+	//p := createPipeline("small.in",
+	//	512,
+	//	4)
+
+	p := createNetworkPipeline("small.in",
+		512, 4)
 	writeToFile(p, "small.out")
-	//printFile("small.out")
+	printFile("small.out")
+
 }
 
 func printFile(filename string) {
@@ -67,6 +72,44 @@ func createPipeline(filename string,
 		sortResults = append(
 			sortResults,
 			node.InMemSort(source))
+	}
+
+	return node.MergeN(sortResults...)
+}
+
+func createNetworkPipeline(filename string,
+	fileSize, chunkCount int) <-chan int {
+
+	chunkSize := fileSize / chunkCount
+
+	node.Init()
+
+	var sortAddr []string
+
+	for i := 0; i < chunkCount; i++ {
+		file, err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+
+		file.Seek(int64(i*chunkSize), 0)
+
+		source := node.ReaderSource(
+			bufio.NewReader(file),
+			chunkSize)
+
+		addr := ":" + strconv.Itoa(7000+i)
+		node.NetworkSink(addr,
+			node.InMemSort(source))
+
+		sortAddr = append(sortAddr, addr)
+	}
+
+	var sortResults []<-chan int
+	for _, addr := range sortAddr {
+		sortResults = append(
+			sortResults,
+			node.NetworkSource(addr))
 	}
 
 	return node.MergeN(sortResults...)
