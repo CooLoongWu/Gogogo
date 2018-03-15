a34c7b854c9eb1b47e2380465f0a375ef80c2728
CooLoongWu <742420210@qq.com>
2018-03-15 15:58:57 +0800

    基本类型学习

--- src/myApp/sayHello.go
+++ src/myApp/sayHello.go
@@ -1,7 +1,16 @@
 package main
 
-import "myApp/utils"
+import (
+	"myApp/utils"
+	"fmt"
+)
 
 func main() {
+	s := "hi"
+	p := &s
+	ss := *p
+
+	fmt.Println("变量s在内存中的地址：", p, "；该内存地址上存放的是：", ss)
+	fmt.Println("表达式s == *(&s)是否成立：", s == *(&s))
 	utils.SayHello()
 }
