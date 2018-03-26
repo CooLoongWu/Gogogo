3544fafc4f0b0364de9ea5e260b398883ae326a2
CooLoongWu <742420210@qq.com>
2018-03-26 17:15:42 +0800

    map切片基础

--- doc/Go-6.md
+++ doc/Go-6.md
@@ -22,3 +22,28 @@ var map1 = map[string]int{}     //声明了一个空map1
 
 map2 := make(map[string]int)    //使用make
 ```
+
+### 键值对是否存在
+如果map中不存在键key，那么获取他的值的时候就会得到一个类型的空值，所以我们就没有办法区分这个key到底是不存在
+还是说这个key的值就是空值。  
+解决办法：
+```go
+val, isExist := map[key]
+```  
+如果isExist是true，那么key存在，且val就是key对应的值。否则key就不存在。
+
+
+### map类型的切片
+如果要获取一个 map 类型的切片，我们必须使用两次 make() 函数，第一次分配切片，第二次分
+配 切片中每个 map 元素，示例代码：
+```go
+items := make([]map[int]int, 5)
+	for i := range items {
+		items[i] = make(map[int]int, 1)
+		items[i][1] = i
+	}
+	fmt.Printf("数据展示： %v\n", items)
+```  
+
+打印结果如下：  
+数据展示: [map[1:0] map[1:1] map[1:2] map[1:3] map[1:4]]；
--- src/myApp/sayHello.go
+++ src/myApp/sayHello.go
@@ -5,13 +5,22 @@ import (
 )
 
 func main() {
-	var map1 = map[string]int{}
-	map2 := make(map[string]int)
 
-	map1["key2"] = 50
-	map2["key1"] = 5
-
-	fmt.Println(map1["key2"])
-	fmt.Println(map2["key1"])
+	ite := map[int]string{1: "hello", 2: "hi"}
+	fmt.Printf("Version A: Value of items: %v\n", ite)
+	// Version A:
+	items := make([]map[int]int, 5)
+	for i := range items {
+		items[i] = make(map[int]int, 1)
+		items[i][1] = i
+	}
+	fmt.Printf("数据展示: %v\n", items)
+	// Version B: NOT GOOD!
+	items2 := make([]map[int]int, 5)
+	for _, item := range items2 {
+		item = make(map[int]int, 1) // item is only a copy of the slice element.
+		item[1] = 2                 // This 'item' will be lost on the next iteration.
+	}
+	fmt.Printf("数据展示: %v\n", items2)
 
 }
