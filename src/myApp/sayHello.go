94f8b89e0b57c16b78d37ea7557c14d8f76ced32
CooLoongWu <742420210@qq.com>
2018-04-13 17:11:37 +0800

    json

--- src/myApp/sayHello.go
+++ src/myApp/sayHello.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"reflect"
 )
 
+type Person struct {
+	name string
+	age  int
+}
+
+type Card struct {
+	id     int
+	Person []*Person
+}
+
 func main() {
-	var str = "hello"
+	p1 := &Person{"李明", 8}
+	p2 := &Person{"李雷", 9}
 
-	v := reflect.ValueOf(str)
-	fmt.Println("v.String", v.String())
+	card := Card{0, []*Person{p1, p2}}
+	js1, _ := json.Marshal(p1)
+	js2, _ := json.Marshal(p2)
 
-	v = reflect.ValueOf(&str)
-	v = v.Elem()
-	v.SetString("hi")
-	fmt.Println("v.String", v.String())
-	fmt.Println("str", str)
+	js3, _ := json.Marshal(card)
+
+	fmt.Printf("%s\n", js1)
+	fmt.Printf("%s\n", js2)
+	fmt.Printf("%s\n", js3)
+
 }
