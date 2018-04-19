42a4c137fc0682bb3c0e7cf6e98222036a8dea40
CooLoongWu <742420210@qq.com>
2018-04-19 17:05:42 +0800

    range的使用

--- doc/Go-3.md
+++ doc/Go-3.md
@@ -96,3 +96,37 @@ HERE:
 fmt.Println("我在4行")
 goto HERE
 ```
+
+## 3.7 Range
+range 关键字用于for循环中迭代数组(array)、切片(slice)、通道(channel)或集合(map)的元素，直接看代码。
+
+```go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//range循环切片
+	nums := []int{1, 2, 3}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("计算结果：", sum)
+
+	//range循环map
+	maps := map[string]string{"a": "Angle", "b": "Bad"}
+	for k, v := range maps {
+		fmt.Println("map循环结果：", "key-value:", k, "-", v)
+	}
+
+	//range循环字符串，打印出来的值是unicode的值
+	str := "Hello"
+	for i, v := range str {
+		fmt.Println("字符串循环结果：", "index-value:", i, "-", v)
+	}
+}
+
+```
--- src/myApp/sayHello.go
+++ src/myApp/sayHello.go
@@ -1,42 +1,27 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 )
 
-type Person struct {
-	Name string           //注意这里结构体变量需要 大写！！
-	Age  int `json:"age"` //如果需要自定义json中的字段名可以在结构体变量后添加`json:"age"`类似的声明
-}
-
 func main() {
-	////序列化
-	//p := Person{
-	//	Name: "LiMing", //赋值的时候也需要将结构体变量名称写出来
-	//	Age:  8,
-	//}
-	//js, _ := json.Marshal(p) //使用json.Marshal()函数来序列化结构体实例
-	//fmt.Printf("%s\n", js)
-	//
-	////反序列化
-	//var person Person
-	//json.Unmarshal(js, &person) //注意第二个参数需要取得对象的地址
-	//fmt.Printf("%+v", person)
+	//range循环切片
+	nums := []int{1, 2, 3}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("计算结果：", sum)
 
-	var s []map[string]interface{}
+	//range循环map
+	maps := map[string]string{"a": "Angle", "b": "Bad"}
+	for k, v := range maps {
+		fmt.Println("map循环结果：", "key-value:", k, "-", v)
+	}
 
-	m1 := map[string]interface{}{"name": "LiMing", "age": 8}
-	m2 := map[string]interface{}{"name": "LiLei", "age": 9}
-	s = append(s, m1)
-	s = append(s, m2)
-
-	js, _ := json.Marshal(s)
-	fmt.Printf("%s\n", js)
-
-	var result []map[string]interface{}
-	json.Unmarshal(js, &result)
-	for key, value := range result {
-		fmt.Println(key, value["age"], value["name"])
+	//range循环字符串，打印出来的值是unicode的值
+	str := "Hello"
+	for i, v := range str {
+		fmt.Println("字符串循环结果：", "index-value:", i, "-", v)
 	}
 }
