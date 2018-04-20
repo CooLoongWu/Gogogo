7eabf4efdc2b0f635d90f4be515f088ff1049698
CooLoongWu <742420210@qq.com>
2018-04-20 17:29:40 +0800

    channel

--- src/myApp/sayHello.go
+++ src/myApp/sayHello.go
@@ -5,23 +5,20 @@ import (
 )
 
 func main() {
-	//range循环切片
-	nums := []int{1, 2, 3}
-	sum := 0
-	for _, num := range nums {
-		sum += num
-	}
-	fmt.Println("计算结果：", sum)
+	num := make(chan int, 2)
 
-	//range循环map
-	maps := map[string]string{"a": "Angle", "b": "Bad"}
-	for k, v := range maps {
-		fmt.Println("map循环结果：", "key-value:", k, "-", v)
-	}
+	fmt.Println(num)
+	num <- 1
 
-	//range循环字符串，打印出来的值是unicode的值
-	str := "Hello"
-	for i, v := range str {
-		fmt.Println("字符串循环结果：", "index-value:", i, "-", v)
-	}
+	var value int
+	value, _ = <-num
+	fmt.Println(value)
+
+	num <- 2
+	value, _ = <-num
+	fmt.Println(value)
+
+	num <- 3
+	value, _ = <-num
+	fmt.Println(value)
 }
