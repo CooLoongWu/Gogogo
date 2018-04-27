de0a77bb981b53d2a8a33462b3616f97dae1f79a
CooLoongWu <742420210@qq.com>
2018-04-27 15:33:08 +0800

    切片容量补充

--- src/myApp/sayHello.go
+++ src/myApp/sayHello.go
@@ -3,20 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	m := make(map[string]string)
-	m["str"] = "string"
-	m["int"] = "int"
+	var s []int
+	s = make([]int, 0)
+	fmt.Println("切片长度：", len(s), "；切片容量：", cap(s))
 
-	fmt.Println("结果:", m["int"])
-	fmt.Println("结果:", m["float"])
+	for i := 0; i < 10; i++ {
+		s = append(s, i)
+		fmt.Println("切片长度：", len(s), "；切片容量：", cap(s))
+	}
 
-	delete(m, "int")
-	fmt.Println("结果:", m["int"])
-
-	var m1 map[string]int
-	m1 = nil
-	fmt.Println("结果：", m1["str"])
-	m1["str"] = 1 //值为nil的map，相当于只读的map，这句写入操作会引发panic
-	fmt.Println("结果：", m1["str"])
-	delete(m1, "str")
 }
