6e547ef56df6c59cfe227f671da6f014bd70db25
CooLoongWu <742420210@qq.com>
2018-04-27 16:45:46 +0800

    channel结合select小技巧

--- src/myApp/sayHello.go
+++ src/myApp/sayHello.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var s []int
-	s = make([]int, 0)
-	fmt.Println("切片长度：", len(s), "；切片容量：", cap(s))
+	ch := make(chan int, 1)
 
-	for i := 0; i < 10; i++ {
-		s = append(s, i)
-		fmt.Println("切片长度：", len(s), "；切片容量：", cap(s))
+	select {
+	case ch <- 1:
+	case ch <- 2:
 	}
 
+	i := <-ch
+	fmt.Println(i)
 }
