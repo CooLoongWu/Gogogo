88844c6084f12576e627d3858c9ea4bd43adc77d
CooLoongWu <742420210@qq.com>
2018-05-04 17:08:33 +0800

    加密

--- src/myApp/sayHello.go
+++ src/myApp/sayHello.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"crypto/md5"
 )
 
-type T struct {
-	Num int       `json:"num"`
-	Str string    `json:"str"`
-	Sli []string  `json:"sli"`
-	Arr [2]string `json:"arr"`
-	Is  bool      `json:"is"`
-	By  []byte    `json:"by"`
-}
-
 func main() {
-	t := T{}
-	js, _ := json.Marshal(t)
-	fmt.Printf("%s", js)
+	str := "This is test ！"
+	md := md5.New()
+	md.Write([]byte(str))
+	res := md.Sum([]byte(""))
+	fmt.Printf("%x\n", res)
 }
