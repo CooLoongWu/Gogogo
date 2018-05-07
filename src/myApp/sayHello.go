63636313f4462afe5bb0e3c7eae4b2c1e28ad786
CooLoongWu <742420210@qq.com>
2018-05-07 15:25:46 +0800

    补充

--- src/myApp/sayHello.go
+++ src/myApp/sayHello.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"fmt"
-	"crypto/md5"
+	"io"
+	"log"
+	"net/http"
 )
 
+const SERVRE_PORT = 8081
+const SERVER_DOMAN = "localhost"
+
+func rootHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello Go !")
+}
+
 func main() {
-	str := "This is test ！"
-	md := md5.New()
-	md.Write([]byte(str))
-	res := md.Sum([]byte(""))
-	fmt.Printf("%x\n", res)
+	http.HandleFunc("/", rootHandler)
+
+	err := http.ListenAndServe(
+		fmt.Sprintf(":%d", SERVRE_PORT),
+		nil)
+
+	//err := http.ListenAndServeTLS(
+	//	fmt.Sprintf(":%d", SERVRE_PORT),
+	//	"rui.crt",
+	//	"rui.key",
+	//	nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
